04e0038f8bfc87ab87387ba79e6dfc0080d8c38b
larisa4642 <larisa4642@yahoo.com>
2017-02-09 17:05:15 +0200

    varianta finala

--- w4/SpeakGoodBye.js
+++ w4/SpeakGoodBye.js
@@ -1,9 +1,9 @@
-
 (function(window){
 	var byeSpeaker={};
 	var speakWord = "Good Bye";
 	byeSpeaker.speak=function(name) {
 	  console.log(speakWord + " " + name);
+	  document.write(speakWord + " " + name+"<br>");
 	}
 	window.byeSpeaker=byeSpeaker;
 
--- w4/SpeakHello.js
+++ w4/SpeakHello.js
@@ -1,9 +1,9 @@
-
 (function(window){
 	var helloSpeaker={};
 	var speakWord = "Hello";
 	helloSpeaker.speak= function (name) {
 	  console.log(speakWord + " " + name);
+	  document.write(speakWord + " " + name+"<br>");
 	}
 	window.helloSpeaker=helloSpeaker;
 })(window);
